e5d79c5d9bba75314989966563be8697d5a2b5bf
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2024-07-19 18:54:41 +0400

    changes for fixinf typescript issues

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -10,8 +10,19 @@ import { Article } from "../../interfaces/Article";
 
 export function CardDefault({ item }: { item: Article }) {
   return (
-    <Card className="mt-6 w-100 mx-4 ">
-      <CardHeader color="blue-gray" className="relative h-56">
+    <Card
+      className="mt-6 w-100 mx-4 "
+      placeholder={undefined}
+      onPointerEnterCapture={undefined}
+      onPointerLeaveCapture={undefined}
+    >
+      <CardHeader
+        color="blue-gray"
+        className="relative h-56"
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
         <img
           src={
             item?.media.length ? item.media[0]["media-metadata"][2]?.url : ""
@@ -19,18 +30,53 @@ export function CardDefault({ item }: { item: Article }) {
           alt={item.title}
         />
       </CardHeader>
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
+      <CardBody
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
+        <Typography
+          variant="h5"
+          color="blue-gray"
+          className="mb-2"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
           {item.title}
         </Typography>
-        <Typography className="mb-4">{item.abstract}</Typography>
-        <Typography variant="paragraph" color="blue-gray">
+        <Typography
+          className="mb-4"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          {item.abstract}
+        </Typography>
+        <Typography
+          variant="paragraph"
+          color="blue-gray"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
           Published on {item.published_date}
         </Typography>
       </CardBody>
-      <CardFooter className="pt-0">
+      <CardFooter
+        className="pt-0"
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
         <a href={item.url} className="inline-block ">
-          <Button>Read More</Button>
+          <Button
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            Read More
+          </Button>
         </a>
       </CardFooter>
     </Card>
