b49a5f3121e81691166cfff7b47dff29e12282d6
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2024-07-21 12:03:18 +0400

    changes in footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="absolute bottom-[0px] w-full bg-gray-800 text-gray-300 p-4">
+    <footer className="absolute bottom-[-100px] left-0 w-full bg-gray-800 text-gray-300 p-4">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 Flash News. All rights reserved.</p>
       </div>
